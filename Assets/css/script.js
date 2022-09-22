c18151f0b7b5151a16da5092443679a53a3dc6d6
macm <mac.mitsch@comcast.net>
2022-09-21 21:30:51 -0500

    added minimal css and wrote the code for UV, wind, temp, and humidity

--- Assets/css/script.js
+++ Assets/css/script.js
@@ -1,7 +1,7 @@
 const apiKey = "6c9f72feb86cd98bc2b2150eabe1f0f9"
 var today = moment().format('L');
 var locationHistory = [];
-function initial() {
+function init () {
     const historyEl = document.getElementById("history");
     let searchHistory = JSON.parse(localstorage.getItem("search-hist")) || [];
     const searchEL = document.getElementById("search");
@@ -12,18 +12,48 @@ function initial() {
     const currentTempEl = document.getElementById("current--temp");
     const currentWindEl = document.getElementById("wind");
     const currentUVEl = document.getElementById("UV");
+    const currentHumidityEL = document.getElementById("Humidity")
     var fiveDayForecastEl = document.getElementById("forecast");
     var todayweatherEL=document.getElementById("current-weather");
 
 }
 
-function cityWeather(city) {
+function cityWeather(cityName) {
     let queryURL = `https://api.openweathermap.org/data/2.5/forecast?lat={lat}&lon={lon}&appid={API key}`;
+    axios.get(queryURL)
+    .then(function(response){
+        console.log(response);
 
-    $.ajax({
-     url:queryURL,
-     method:"GET"
-    }).then(function(cityWeatherResponse){
-        console.log(cityWeatherResponse);
-    })
-};
+    const currentDate = new Date(response.data.dt*1000);
+    console.log(currentDate);
+        
+    const day= currentDate.getDate();
+    const month= currentDate.getMonth();
+    const year = currentDate.getFullYear();
+        // formats how the weather is displayed
+        nameEl.innerHTML = response.data.name + "("+ month + "/" + day + "/" + year + ")"; 
+        
+        // formats and displays the weather forcast
+        let wetPic = response.data.weather[0].icon;
+        currentPicEl.setAttribute("src,https://openweathermap.org/img/wn/" + wetPic + "@2x.png");
+        currentPicEl.setAttribute("alt",response.data.weather[0].description);
+
+        currentTempEl.innerHTML = "Temperature: " + k2f(response.data.main.temp) + "&#176F";
+        currentWindEl.innerHTML = "Wind: " + response.data.wind.speed + "MPH";
+        currentHumidityEL.innerHTML = "Humidity: " + respomse.data.humidity + "%";
+
+        let latitude = response.data.coord.lat;
+        let longitude = respomse.data.coord.lon;
+        let UVQueryURL = "https://api.openweathermap.org/data/2.5/uvi/forecast?lat=" + lat + "&lon=" + lon + "&appid=" + apiKey + "&cnt=1";
+        axios.get(UVQueryURL)
+        .then(function(response){
+            let UVIndex = document.createElement("span");
+            UVIndex.setAttribute("class","badge badge-danger");
+            UVIndex.innerHTML = response.data[0].value;
+            currentUVEl.innerHTML = "UV: ";
+            currentUVEl.append(UVIndex);
+        });
+        
+    
+}
+init();
